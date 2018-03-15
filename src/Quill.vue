bc0b1efcd8665fe1a116b5bdb085015a91286310
Jörg Richter <jri@deepamehta.de>
2018-03-15 23:03:11 +0100

    Support the "bubble" theme
    Depending on the selected theme only the required CSS is loaded

--- src/Quill.vue
+++ src/Quill.vue
@@ -4,18 +4,18 @@
 
 <script>
 import Quill from 'quill'
-import 'quill/dist/quill.snow.css'
 
-var emitted = false
+var cssReady            // a promise resolved once CSS is loaded
+var emitted = false     // tracks if "quill-imported" event emitted already
 
 export default {
 
   props: ['value', 'options'],
 
   created () {
-    // TODO: why is this component sometimes instantiated in detail panel "info" mode?
-    console.log('quill created', emitted, this.$store.state.details.mode)
-    // emit only once
+    // DM5 Webclient TODO: why is this component sometimes instantiated in detail panel "info" mode?
+    // console.log('quill created', emitted, this.$store.state.details.mode)
+    cssReady = this.loadCSS()
     if (!emitted) {
       this.$emit('quill-imported', Quill)
       emitted = true
@@ -23,13 +23,29 @@ export default {
   },
 
   mounted () {
-    const quill = new Quill(this.$refs.container, this.options)
-    quill.pasteHTML(this.value)
-    quill.on('text-change', () => {
-      const html = quill.root.innerHTML
-      this.$emit('input', html)
+    cssReady && cssReady.then(() => {
+      const quill = new Quill(this.$refs.container, this.options)
+      quill.pasteHTML(this.value)
+      quill.on('text-change', () => {
+        const html = quill.root.innerHTML
+        this.$emit('input', html)
+      })
+      this.$emit('quill-ready', quill)
     })
-    this.$emit('quill-ready', quill)
+  },
+
+  methods: {
+    loadCSS () {
+      const theme = this.options.theme
+      if (theme === 'snow') {
+        return import('quill/dist/quill.snow.css'   /* webpackChunkName: "quill-snow" */)
+      } else if (theme === 'bubble') {
+        return import('quill/dist/quill.bubble.css' /* webpackChunkName: "quill-bubble" */)
+      } else {
+        // TODO: custom themes
+        throw Error(`"${theme}" is an unexpected Quill theme`)
+      }
+    }
   }
 }
 </script>
