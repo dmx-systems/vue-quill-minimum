7fe32f518d72aebee3a81719f3561bdfd6aba9e0
Jörg Richter <jri@deepamehta.de>
2019-12-04 16:53:51 +0100

    Add `build` and `stats` scripts

---
+++ webpack.config.js
@@ -0,0 +1,31 @@
+const { VueLoaderPlugin } = require('vue-loader')
+
+module.exports = {
+  entry: './src/Quill.vue',
+  output: {
+    filename: 'vue-qill-minimum.min.js',
+    library: 'VueQuillMinimum',
+    libraryTarget: 'umd'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.vue$/,
+        loader: 'vue-loader'
+      },
+      {
+        test: /\.css$/,
+        loader: ['style-loader', 'css-loader']
+      }
+    ]
+  },
+  plugins: [
+    new VueLoaderPlugin()
+  ],
+  stats: {
+    entrypoints: false,
+  },
+  performance: {
+    hints: false
+  }
+}
